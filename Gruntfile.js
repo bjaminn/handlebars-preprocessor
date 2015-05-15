cc63ff92a47f680ee30d03dc39739026f7f45a56
Benjamin Nave <bhnave@ra.rockwell.com>
2015-05-15 14:10:38 -0400

    Graphviz Templating
    Add support for helper functions
    Add graphviz support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,37 @@
-var outputFile = "templates.js"
+var inputFile = "templates.hbs"
+var outputFile = "templates.dot"
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   'use strict';
 
   // load grunt tasks
   grunt.loadNpmTasks('grunt-compile-handlebars');
+  grunt.loadNpmTasks('grunt-graphviz');
 
-  grunt.registerTask('delete-output', function() {
+  grunt.registerTask('delete-output', function () {
     grunt.log.writeln("Deleting " + outputFile);
     grunt.file.delete(outputFile, false);
   });
 
-  grunt.registerTask('default', ['delete-output', 'compile-handlebars'])
+  grunt.registerTask('default', ['delete-output', 'compile-handlebars', 'graphviz'])
 
   // create configureation object
   grunt.initConfig({
+    graphviz: {
+      your_target: {
+        files: {
+          'templates.png': outputFile,
+          // etc... 
+        }
+      },
+    },
     'compile-handlebars': {
       all: {
         files: [{
           dest: outputFile,
-          src: "templates.hbs"
+          src: inputFile
         }],
-        helpers: ['for.js'],
+        helpers: ['node_modules/handlebars-helpers/lib/helpers/helpers-math.js', 'helpers/*.js'],
         templateData: {
           books: [{
             title: "A Brave New World"
@@ -31,3 +41,4 @@ module.exports = function(grunt) {
     }
   });
 }
+
